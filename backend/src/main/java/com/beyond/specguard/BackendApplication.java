018e1ecce51d3bb56cd804f796aff8642ad5c76b
dobbyRR <zxr1033@gmail.com>
2025-08-28 15:09:35 +0900

    no message

--- backend/src/main/java/com/beyond/specguard/BackendApplication.java
+++ backend/src/main/java/com/beyond/specguard/BackendApplication.java
@@ -2,8 +2,10 @@ package com.beyond.specguard;
 
 import org.springframework.boot.SpringApplication;
 import org.springframework.boot.autoconfigure.SpringBootApplication;
+import org.springframework.scheduling.annotation.EnableScheduling;
 
 @SpringBootApplication
+@EnableScheduling
 public class BackendApplication {
 
     public static void main(String[] args) {
@@ -11,3 +13,4 @@ public class BackendApplication {
     }
 
 }
+
