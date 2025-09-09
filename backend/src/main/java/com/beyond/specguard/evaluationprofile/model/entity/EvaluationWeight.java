a20bad42e91acc4775d2936233f99d85701d94b3
viroovr <tjgusdnjs789@gmail.com>
2025-09-09 15:28:38 +0900

    feat KEYWORD_MATCH 제거

--- backend/src/main/java/com/beyond/specguard/evaluationprofile/model/entity/EvaluationWeight.java
+++ backend/src/main/java/com/beyond/specguard/evaluationprofile/model/entity/EvaluationWeight.java
@@ -1,7 +1,22 @@
 package com.beyond.specguard.evaluationprofile.model.entity;
 
-import jakarta.persistence.*;
-import lombok.*;
+import jakarta.persistence.Column;
+import jakarta.persistence.ConstraintMode;
+import jakarta.persistence.Entity;
+import jakarta.persistence.EnumType;
+import jakarta.persistence.Enumerated;
+import jakarta.persistence.FetchType;
+import jakarta.persistence.ForeignKey;
+import jakarta.persistence.GeneratedValue;
+import jakarta.persistence.Id;
+import jakarta.persistence.JoinColumn;
+import jakarta.persistence.ManyToOne;
+import jakarta.persistence.Table;
+import lombok.AccessLevel;
+import lombok.AllArgsConstructor;
+import lombok.Builder;
+import lombok.Getter;
+import lombok.NoArgsConstructor;
 import org.hibernate.annotations.CreationTimestamp;
 import org.hibernate.annotations.UpdateTimestamp;
 
@@ -66,7 +81,6 @@ public class EvaluationWeight {
         VELOG_RECENT_ACTIVITY,
         VELOG_KEYWORD_MATCH,
         CAREER_MATCH,
-        CERTIFICATE_MATCH,
-        KEYWORD_MATCH
+        CERTIFICATE_MATCH
     }
 }
