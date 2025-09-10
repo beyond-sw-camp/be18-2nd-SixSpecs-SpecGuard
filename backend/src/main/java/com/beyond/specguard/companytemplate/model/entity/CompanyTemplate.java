ce59e4b6eef5187489253d81850b04939aaa1b28
sangwon5579 <81066249+sangwon5579@users.noreply.github.com>
2025-09-10 15:45:28 +0900

    [fix] 누락된 import문 추가

--- backend/src/main/java/com/beyond/specguard/companytemplate/model/entity/CompanyTemplate.java
+++ backend/src/main/java/com/beyond/specguard/companytemplate/model/entity/CompanyTemplate.java
@@ -17,6 +17,7 @@ import jakarta.persistence.Id;
 import jakarta.persistence.JoinColumn;
 import jakarta.persistence.ManyToOne;
 import jakarta.persistence.OneToMany;
+import jakarta.persistence.Index;
 import jakarta.persistence.Table;
 import lombok.AccessLevel;
 import lombok.AllArgsConstructor;
