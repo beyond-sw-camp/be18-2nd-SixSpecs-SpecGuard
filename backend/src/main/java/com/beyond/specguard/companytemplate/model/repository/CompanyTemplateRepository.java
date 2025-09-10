9cba675961b3733bc221574f876acd2c4d9f0892
sangwon5579 <81066249+sangwon5579@users.noreply.github.com>
2025-09-10 15:25:19 +0900

    [feature] resume delete를 위한 endDate가 now보다 지난 id 가져오기

--- backend/src/main/java/com/beyond/specguard/companytemplate/model/repository/CompanyTemplateRepository.java
+++ backend/src/main/java/com/beyond/specguard/companytemplate/model/repository/CompanyTemplateRepository.java
@@ -3,8 +3,17 @@ package com.beyond.specguard.companytemplate.model.repository;
 import com.beyond.specguard.companytemplate.model.entity.CompanyTemplate;
 import org.springframework.data.jpa.repository.JpaRepository;
 import org.springframework.data.jpa.repository.JpaSpecificationExecutor;
-
+import java.time.LocalDateTime;
+import java.util.List;
+import org.springframework.data.jpa.repository.Query;
 import java.util.UUID;
 
 public interface CompanyTemplateRepository extends JpaRepository<CompanyTemplate, UUID>, JpaSpecificationExecutor<CompanyTemplate> {
+  @Query("""
+        select t.id
+        from CompanyTemplate t
+        where t.endDate is not null
+          and t.endDate < :now
+    """)
+    List<UUID> findExpiredTemplateIds(LocalDateTime now);
 }
