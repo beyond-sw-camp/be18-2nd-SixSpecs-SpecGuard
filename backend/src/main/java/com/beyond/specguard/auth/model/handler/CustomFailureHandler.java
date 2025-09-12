95b6104ea0c45315ec3101e7cbcb5534f89eb1a3
viroovr <tjgusdnjs789@gmail.com>
2025-09-12 17:49:53 +0900

    refactor directory

--- backend/src/main/java/com/beyond/specguard/auth/model/handler/CustomFailureHandler.java
+++ backend/src/main/java/com/beyond/specguard/auth/model/handler/CustomFailureHandler.java
@@ -3,13 +3,13 @@ package com.beyond.specguard.auth.model.handler;
 import com.beyond.specguard.auth.exception.errorcode.AuthErrorCode;
 import com.beyond.specguard.common.exception.ErrorResponse;
 import com.fasterxml.jackson.databind.ObjectMapper;
+import jakarta.servlet.http.HttpServletRequest;
+import jakarta.servlet.http.HttpServletResponse;
 import lombok.extern.slf4j.Slf4j;
 import org.springframework.security.core.AuthenticationException;
 import org.springframework.security.web.authentication.AuthenticationFailureHandler;
 import org.springframework.stereotype.Component;
 
-import jakarta.servlet.http.HttpServletRequest;
-import jakarta.servlet.http.HttpServletResponse;
 import java.io.IOException;
 
 @Slf4j
--- backend/src/main/java/com/beyond/specguard/auth/model/handler/CustomSuccessHandler.java
+++ backend/src/main/java/com/beyond/specguard/auth/model/handler/CustomSuccessHandler.java
@@ -1,8 +1,8 @@
 package com.beyond.specguard.auth.model.handler;
 
 import com.beyond.specguard.admin.model.service.InternalAdminDetails;
-import com.beyond.specguard.auth.model.service.RedisTokenService;
 import com.beyond.specguard.auth.model.service.CustomUserDetails;
+import com.beyond.specguard.auth.model.service.RedisTokenService;
 import com.beyond.specguard.common.util.CookieUtil;
 import com.beyond.specguard.common.util.JwtUtil;
 import jakarta.servlet.ServletException;
