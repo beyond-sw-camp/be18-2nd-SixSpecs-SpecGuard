5d2b6e5b10b5046c4c4fb41805e9b3be3d68bd56
viroovr <tjgusdnjs789@gmail.com>
2025-09-18 09:27:36 +0900

    fix SUCCESS -> COMPLETED

--- backend/src/main/java/com/beyond/specguard/certificate/model/entity/CertificateVerification.java
+++ backend/src/main/java/com/beyond/specguard/certificate/model/entity/CertificateVerification.java
@@ -1,7 +1,21 @@
 package com.beyond.specguard.certificate.model.entity;
 
 import com.beyond.specguard.resume.model.entity.core.ResumeCertificate;
-import jakarta.persistence.*;
+import jakarta.persistence.Column;
+import jakarta.persistence.ConstraintMode;
+import jakarta.persistence.Entity;
+import jakarta.persistence.EnumType;
+import jakarta.persistence.Enumerated;
+import jakarta.persistence.FetchType;
+import jakarta.persistence.ForeignKey;
+import jakarta.persistence.GeneratedValue;
+import jakarta.persistence.GenerationType;
+import jakarta.persistence.Id;
+import jakarta.persistence.JoinColumn;
+import jakarta.persistence.ManyToOne;
+import jakarta.persistence.PrePersist;
+import jakarta.persistence.PreUpdate;
+import jakarta.persistence.Table;
 import lombok.AllArgsConstructor;
 import lombok.Builder;
 import lombok.Getter;
@@ -47,7 +61,7 @@ public class CertificateVerification {
     private LocalDateTime updatedAt;
 
     public void setStatusSuccess() {
-        this.status = Status.SUCCESS;
+        this.status = Status.COMPLETED;
     }
     public void setStatusFailed() {
         this.status = Status.FAILED;
@@ -70,6 +84,6 @@ public class CertificateVerification {
     }
 
     public enum Status {
-        PENDING, SUCCESS, FAILED
+        PENDING, COMPLETED, FAILED
     }
 }
