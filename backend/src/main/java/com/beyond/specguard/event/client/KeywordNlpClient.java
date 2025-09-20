22c01553b916d3dd0a15f7665b80a588e55e9055
amicitia3600 <dyungwoo3600@gmail.com>
2025-09-21 04:29:42 +0900

    [feature] nlp 정상작동 확인

--- backend/src/main/java/com/beyond/specguard/event/client/KeywordNlpClient.java
+++ backend/src/main/java/com/beyond/specguard/event/client/KeywordNlpClient.java
@@ -2,9 +2,11 @@ package com.beyond.specguard.event.client;
 
 import lombok.RequiredArgsConstructor;
 import lombok.extern.slf4j.Slf4j;
+import org.springframework.core.ParameterizedTypeReference;
 import org.springframework.stereotype.Component;
 import org.springframework.web.reactive.function.client.WebClient;
 
+import java.util.Map;
 import java.util.UUID;
 
 @Component
@@ -19,7 +21,7 @@ public class KeywordNlpClient {
                     .uri("/nlp/crawlingKeyword")
                     .bodyValue(new StartBody(resumeId.toString()))
                     .retrieve()
-                    .bodyToMono(Void.class) // 응답 바디는 안 써도 되니까 Void
+                    .bodyToMono(new ParameterizedTypeReference<Map<String, Object>>() {})
                     .block();
 
             log.info("NLP 키워드 추출 요청 완료 - resumeId={}", resumeId);
